ed4c914526506f261a8293d18f7b249205da7ead
lalitha31084 <148032121+lalitha31084@users.noreply.github.com>
2025-12-30 13:59:51 +0530

    Final Submission for Multi-Tenant-Saas challenge

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,14 @@
 const { Pool } = require('pg');
-
 const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // If we are in production (Render), use SSL. If localhost, disable it.
-  ssl: isProduction 
-    ? { rejectUnauthorized: false } 
-    : false
+  ssl: isProduction ? { rejectUnauthorized: false } : false
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
 });
 
 module.exports = {
